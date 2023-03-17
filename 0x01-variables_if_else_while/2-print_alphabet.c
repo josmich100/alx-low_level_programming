c709eb700bee8a735d7fd1c0f7ecda18e3e035f7
Josmich100 <michinajosiah@gmail.com>
2023-03-17 12:28:08 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char charr;
+
+for (charr = 'a'; charr <= 'z'; charr++)
+{
+putchar(charr);
+}
+
+putchar('\n');
+
+/* your code goes there */
+return (0);
+} 
