79bf4a2333c5e4604d81963589ebd42c21e8deb2
Josmich100 <michinajosiah@gmail.com>
2023-04-26 01:37:50 -0700

    saveds

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,64 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * check_digits - check if the given string is composed only of digits
+ *
+ * @str: the string to check
+ *
+ * Return: 1 if str contains only digits, 0 otherwise
+ */
+int check_digits(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (!isdigit(str[i]))
+			return (0);
+
+		i++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - multiply two positive numbers
+ *
+ * @argc: the number of arguments
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	int num1, num2, product;
 
+	/* Check the number of arguments */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+
+	/* Check if the arguments are valid */
+	if (!check_digits(argv[1]) || !check_digits(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	/* Convert arguments to integers */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	/* Multiply the numbers */
+	product = num1 * num2;
+
+	/* Print the result */
+	printf("%d\n", product);
+
+	return (0);
 }
