36c69cf9afcfe2373818fb74ec0d681c97878b07
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-12-13 23:29:32 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,39 +1,41 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array
- *                 of integers using binary search.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
+ * binary_search - Searches for a value in a sorted array using binary search.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: The number of elements in @array.
  * @value: The value to search for.
  *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the index where the value is located.
+ * Return: The index where the value is located, or -1 if not found.
  *
- * Description: Prints the [sub]array being searched after each change.
+ * Description: Prints the array being searched every time it changes.
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i, left, right;
+    size_t left, right, mid;
 
     if (array == NULL)
-        return (-1);
+        return -1;
 
-    for (left = 0, right = size - 1; right >= left;)
+    left = 0;
+    right = size - 1;
+
+    while (left <= right)
     {
         printf("Searching in array: ");
-        for (i = left; i < right; i++)
-            printf("%d, ", array[i]);
-        printf("%d\n", array[i]);
+        for (mid = left; mid < right; mid++)
+            printf("%d, ", array[mid]);
+        printf("%d\n", array[mid]);
 
-        i = left + (right - left) / 2;
-        if (array[i] == value)
-            return (i);
-        if (array[i] > value)
-            right = i - 1;
+        mid = left + (right - left) / 2;
+
+        if (array[mid] == value)
+            return mid;
+        else if (array[mid] < value)
+            left = mid + 1;
         else
-            left = i + 1;
+            right = mid - 1;
     }
 
-    return (-1);
+    return -1;  /* Value not found */
 }
